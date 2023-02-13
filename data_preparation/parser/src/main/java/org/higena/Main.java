92ec813d20b6b57765e12fb6efaf08e6d7f7fabf
Ana InAas Oliveira de Barros <up201806593@fe.up.pt>
2023-02-13 10:25:19 +0000

    :sparkles: Add: Parser jar

--- data_preparation/parser/src/main/java/org/higena/Main.java
+++ data_preparation/parser/src/main/java/org/higena/Main.java
@@ -4,10 +4,20 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String code = "\nvar sig File {\n\tvar link : lone File\n}\nvar sig Trash in File {}\nvar sig Protected in File {}\npred prop1 {\n\n}\npred prop2 {\n\n}\npred prop3 {\n\talways some File\n}\npred prop4 {\n\t\n  \teventually some Trash\n}\npred prop5 {\n\t\n\t\n\t \n}\npred prop6 {\n\talways all f:File | f in Trash implies after f in Trash\n}\t\npred prop7 {\n\t\n  \teventually some Protected\n}\npred prop8 {\n\t\n}\npred prop9 {\n\talways all p:Protected | p not in Trash\n}\npred prop10 {\n\t\n}\npred prop11 {\n\talways all f:File | f in File-Protected implies after f in Protected\n}\npred prop12 {\n\t\n}\npred prop13 {\n\n}\npred prop14 {\n\n}\npred prop15 {\n\n}\npred prop16 {\n\n}\npred prop17 {\n\n}\npred prop18 {\n\n}\npred prop19 {\n\n}\npred prop20 {\n\n}";
 
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar a4f.jar <code>");
+            System.exit(1);
+        }
+
+        // Parse the code
+        String code = args[0];
         Map<String, A4FAst> asts = A4FParser.parse(code);
-        //System.out.println(asts.get("this/prop4"));
+
+        // Print the ASTs
+        for (A4FAst ast : asts.values()) {
+            System.out.println(ast);
+        }
     }
 }
 
