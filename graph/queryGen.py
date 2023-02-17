e721bc0601ca1549d12c74d4e0d2273862d1782f
Ana Barros <up201806593@fe.up.pt>
2023-02-17 14:55:12 +0000

    ✨ (Add): script load query generator for each dataset

--- graph/queryGen.py
+++ graph/queryGen.py
@@ -1,30 +1,53 @@
+import pandas as pd
+import os
+
 
 def getLoadQuery(file, columns):
-    load = f"""
-            //Load gAeD3MTGCCv8YNTaK
-            LOAD CSV WITH HEADERS FROM 'file:///{file}.csv' AS row"""
+
+    commonColumns = ["cmd_i", "sat", "_id", "code", "derivationOf"]
+    columns = [col for col in columns if col not in commonColumns]
+
+    load = "\n//Load gAeD3MTGCCv8YNTaK\nLOAD CSV WITH HEADERS FROM 'file:///" + file + \
+        ".csv' AS row\n" + \
+        "WITH toInteger(row.cmd_i) AS cmd_i, \n\ttoInteger(row.sat) AS sat," + \
+        "row._id AS id, \n\trow.code AS code, \n\trow.derivationOf AS derivationOf"
 
     for col in columns:
+        load += ",\n\trow." + col + " AS " + col
+
+    load += "\nMERGE (s:Submission {" + \
+        "\n\tid: id," + \
+        "\n\tcmd_i: cmd_i," + \
+        "\n\tcode: code," + \
+        "\n\tsat: sat," + \
+        "\n\tderivationOf: derivationOf"
+
+    for col in columns:
+        load += ",\n\t" + col + ": " + col
+
+    load += "\n})"
+
+    return load
 
 
-            WITH toInteger(row.cmd_i) AS cmd_i, toInteger(row.sat) AS sat, 
-                row._id AS id, row.code AS code, row.derivationOf AS derivationOf,
-                row.undirected AS undirected, row.weaklyConnected AS  weaklyConnected,
-                row.stonglyConnected AS  stonglyConnected, row.transitive AS  transitive, 
-                row.acyclic AS  acyclic, row.complete AS  complete,
-                row.oriented AS  oriented, row.noLoops AS  noLoops
-            MERGE (s:Submission {
-                id: id,
-                cmd_i: cmd_i,
-                sat: sat,
-                derivationOf: derivationOf,
-                code: code,
-                undirected: undirected,
-                weaklyConnected: weaklyConnected,
-                stronglyConnected: stonglyConnected,
-                transitive: transitive,
-                acyclic: acyclic,
-                complete: complete,
-                oriented: oriented,
-                noLoops: noLoops
-            })"""
+def importDataFromDir(dir):
+    # Import all the data from a directory
+    # dir: directory with the data
+    # return: a dictionary of dataframes
+    dict = {}
+
+    for file in os.listdir(dir):
+        if file.endswith('.csv'):
+            df = pd.read_csv(f'{dir}/' + file)
+            dict[file.removesuffix(".csv")] = df
+            print(f"Imported {file}.")
+
+    return dict
+
+
+# Import files from the data directory
+dict = importDataFromDir('../data_preparation/data/prepared')
+
+file = "9jPK8KBWzjFmBx4Hb"
+query = getLoadQuery(file, dict[file].columns)
+print(query)
