1ae75d1a5ad709b2b8e92a0da5c4da52f35450da
Ana Barros <up201806593@fe.up.pt>
2023-03-02 10:02:35 +0000

    📝 (Add): db functions documentation

--- graph/higena/src/main/java/org/example/graph/Db.java
+++ graph/higena/src/main/java/org/example/graph/Db.java
@@ -16,6 +16,9 @@ public class Db implements AutoCloseable {
     session = driver.session(SessionConfig.forDatabase(database));
   }
 
+  /**
+   *
+   */
   public void setup() {
     deleteAllNodes();
     addUniqueConstraint();
@@ -24,25 +27,84 @@ public class Db implements AutoCloseable {
     deleteProperty("derivationOf");
     addCorrectLabel();
     addIncorrectLabel();
+    aggregateEquivNodes("ast");
   }
 
+  /**
+   * Runs a query and returns the result.
+   * @param query Query to run
+   * @return Result of the query
+   */
   public Result runQuery(String query) {
     return session.run(new Query(query));
   }
 
+  /**
+   * Close the driver and session.
+   * @throws RuntimeException
+   */
   public void close() throws RuntimeException {
     driver.close();
   }
 
+  /**
+   * Verifies that the connection to the database is working.
+   */
   public void verifyConnection() {
     driver.verifyConnectivity();
   }
 
+  /**
+   * Checks if projection exists.
+   * @param name Name of the projection
+   * @return True if projection exists, false otherwise
+   */
   public boolean hasProjection(String name) {
     Result res = runQuery("CALL gds.graph.exists('" + name + "') YIELD exists");
     return res.single().get("exists").asBoolean();
   }
 
+  // Algorithms
+
+  /**
+   * Creates edges between nodes with the same property. Then, runs the
+   * Weakly Connected Components algorithm to find the connected components
+   * of the graph. Each node gets a componentId property with the id of the
+   * component it belongs to.
+   *
+   * @param property Name of the property to aggregate by
+   */
+  public void aggregateEquivNodes(String property) {
+    String projectionName = "equalGraph";
+    String relName = "EQUAL";
+    // Create edges between nodes with the same property
+    runQuery("""
+            MATCH (n:Submission)
+            MATCH (s:Submission)
+            WHERE n._id <> s._id AND n.%s = s.%s
+            MERGE (n)-[:%s]-(s)""".formatted(property, property, relName));
+    // Check if graph already exists
+    if (!hasProjection(projectionName)) {
+      // Create graph projection
+      addProjection(projectionName, "Submission", relName);
+    }
+    // Run Weakly Connected Components algorithm
+    runConnectedComponents(projectionName, "componentId");
+    // Drop graph projection
+    deleteProjection(projectionName);
+    // Drop edges
+    deleteEdges(relName);
+    // Aggregate nodes: delete equivalent nodes except for one
+    deleteEquivNodes();
+  }
+
+  /**
+   * Runs the weakly connected components algorithm on the graph projection
+   * and writes the component id of each node to the indicated property.
+   *
+   * @param graphName     Name of the graph projection
+   * @param writeProperty Name of the property to write the component id to
+   */
   public void runConnectedComponents(String graphName, String writeProperty) {
     runQuery("CALL gds.wcc.write('" + graphName + "', {writeProperty: '" +
             writeProperty + "'}) " + "YIELD " +
@@ -51,10 +113,18 @@ public class Db implements AutoCloseable {
 
   // CREATE methods
 
+  /**
+   * Creates a new database with the given name.
+   *
+   * @param name Name of the database
+   */
   public void addDb(String name) {
     runQuery("CREATE DATABASE " + name + " IF NOT EXISTS");
   }
 
+  /**
+   * Adds constraint to ensure that each node has a unique _id property.
+   */
   public void addUniqueConstraint() {
     runQuery("""
             CREATE CONSTRAINT UniqueSubmission
@@ -64,6 +134,12 @@ public class Db implements AutoCloseable {
     System.out.println("Created unique constraint.");
   }
 
+  /**
+   * Loads nodes from a csv file with Alloy4Fun submissions into the database.
+   *
+   * @param challenge Challenge/Folder name (e.g. 9jPK8KBWzjFmBx4Hb)
+   * @param pred      Predicate name/File name (e.g. prop1) without extension
+   */
   public void addNodes(String challenge, String pred) {
     Result res = runQuery("LOAD CSV WITH HEADERS FROM 'file:///" + challenge +
             "/" + pred + ".csv' AS row\n" +
@@ -81,6 +157,10 @@ public class Db implements AutoCloseable {
     System.out.println("Created " + res.consume().counters().nodesCreated() + " nodes.");
   }
 
+  /**
+   * Creates undirected Derives edges between nodes where the derivationOf
+   * property of the source node matches the _id property of the target node.
+   */
   public void addEdges() {
     Result res = runQuery("""
             MATCH (s:Submission)
@@ -92,6 +172,9 @@ public class Db implements AutoCloseable {
             " Derives edges.");
   }
 
+  /**
+   * Adds a Correct label to all nodes with a sat property of 1.
+   */
   public void addCorrectLabel() {
     Result res = runQuery("""
             MATCH (s:Submission {sat: 1})
@@ -101,6 +184,9 @@ public class Db implements AutoCloseable {
             "Correct labels.");
   }
 
+  /**
+   * Adds an Incorrect label to all nodes with a sat property of 0.
+   */
   public void addIncorrectLabel() {
     Result res = runQuery("""
             MATCH (s:Submission {sat: 0})
@@ -110,6 +196,14 @@ public class Db implements AutoCloseable {
             "Incorrect labels.");
   }
 
+  /**
+   * Creates a graph projection with the given name, label, and relationship.
+   * Graph projections are used to run neo4j graph data science algorithms.
+   *
+   * @param name         Name of the graph projection
+   * @param label        Label of the nodes
+   * @param relationship Relationship type of the edges
+   */
   public void addProjection(String name, String label, String relationship) {
     runQuery("CALL gds.graph.project('%s', '%s', '%s')"
             .formatted(name, label, relationship));
@@ -117,11 +211,18 @@ public class Db implements AutoCloseable {
 
   // DELETE methods
 
+  /**
+   * Deletes all nodes and edges from the database.
+   */
   public void deleteAllNodes() {
     runQuery("MATCH (n) DETACH DELETE n");
     System.out.println("Delete all nodes.");
   }
 
+  /**
+   * Deletes property from all nodes.
+   * @param property Name of the property to delete
+   */
   public void deleteProperty(String property) {
     Result res = runQuery("MATCH (s:Submission)\n" +
             "REMOVE s." + property + "\n" +
@@ -130,10 +231,18 @@ public class Db implements AutoCloseable {
             " " + property + " properties.");
   }
 
+  /**
+   * Deletes the graph projection with the given name.
+   * @param name Name of the graph projection
+   */
   public void deleteProjection(String name) {
     runQuery("CALL gds.graph.drop('" + name + "')");
   }
 
+  /**
+   * Deºetes all edges of the given relationship type.
+   * @param relationship Relationship type of the edges to delete
+   */
   public void deleteEdges(String relationship) {
     Result res = runQuery("MATCH ()-[r:" + relationship + "]-()\n" +
             "DELETE r\n" +
@@ -141,10 +250,19 @@ public class Db implements AutoCloseable {
     System.out.println("Deleted " + res.consume().counters().relationshipsDeleted() + " edges.");
   }
 
+    /**
+     * Deletes the database with the given name.
+     * @param name Name of the database to delete
+     */
   public void deleteDb(String name) {
     runQuery("DROP DATABASE " + name + " IF EXISTS");
   }
 
+    /**
+     * Deletes all loops of the given relationship type. Loops are edges
+     * where the source and target node are the same.
+     * @param relationship Relationship type of the loops to delete
+     */
   public void deleteLoops(String relationship) {
     Result res = runQuery("MATCH (s:Submission)-[r:" + relationship + "]->" +
             "(s:Submission)\n" +
@@ -153,30 +271,67 @@ public class Db implements AutoCloseable {
     System.out.println("Deleted " + res.consume().counters().relationshipsDeleted() + " loops.");
   }
 
-  public void deleteEquivNodes(int componentId) {
-    List<String> queries = getDelEquivNodesQueries(componentId);
-    for (String query : queries) {
-      runQuery(query);
+    /**
+     * Deletes equivalent nodes. Equivalent nodes are nodes that belong to the
+     * same component after running the connected components algorithm.
+     * For each component the first node is kept and all other nodes are deleted.
+     * Derivations of deleted nodes are updated to point to the first node of the
+     * component.
+     */
+  public void deleteEquivNodes() {
+    Result rest = getDistinctPropertyValues("componentId");
+    // Iterate over all components
+    while (rest.hasNext()) {
+      int componentId = rest.next().get("componentId").asInt();
+      // Remove equivalent nodes from component
+      List<String> queries = getDelEquivNodesQueries(componentId);
+      for (String query : queries) {
+        runQuery(query);
+      }
     }
+    // Remove resulting loops
+    deleteLoops("Derives");
+    // Delete componentId property
+    deleteProperty("componentId");
   }
 
   // GET methods
 
+  /**
+   * Returns all nodes with the given property value.
+   * @param property Name of the property
+   * @param value Value of the property
+   * @return Result object containing all nodes with the given property value
+   */
   public Result getNodesWithPropertyValue(String property, int value) {
     return runQuery("MATCH (s:Submission)\n" +
             "WHERE s." + property + " = " + value + " RETURN s");
   }
 
+    /**
+     * Returns all distinct values of the given property.
+     * @param property Name of the property
+     * @return Result object containing all distinct values of the given property
+     */
   public Result getDistinctPropertyValues(String property) {
     return runQuery("MATCH (s:Submission)\n" +
             "RETURN DISTINCT s." + property + " AS " + property);
   }
 
+  /**
+   * Returns the name of the database.
+   * @return Name of the database
+   */
   public String getName() {
     return name;
   }
 
-  public List<String> getDelEquivNodesQueries(int componentId) {
+  /**
+   * Auxiliary method to create queries for deleting equivalent nodes.
+   * @param componentId Id of the component
+   * @return List of queries
+   */
+  private List<String> getDelEquivNodesQueries(int componentId) {
     List<String> queries = new ArrayList<>();
     // Get all nodes in component and the first node of the component
     String mainQuery = """
@@ -196,16 +351,16 @@ public class Db implements AutoCloseable {
                 DELETE r
             }
             """
-            );
+    );
     // Delete all component nodes except first node
     queries.add(mainQuery + """
-                CALL {
-                    WITH cN, firstN
-                    MATCH (cN)
-                    WHERE cN <> firstN
-                    DETACH DELETE cN
-                }
-                """);
+            CALL {
+                WITH cN, firstN
+                MATCH (cN)
+                WHERE cN <> firstN
+                DETACH DELETE cN
+            }
+            """);
     return queries;
   }
 }
--- graph/higena/src/main/java/org/example/graph/Graph.java
+++ graph/higena/src/main/java/org/example/graph/Graph.java
@@ -22,63 +22,6 @@ public class Graph {
     }
   }
 
-  public void mergeEquivNodes(String property) {
-    aggregateEquivNodes(property);
-    removeEquivNodes();
-  }
-
-  /**
-   * Removes nodes with repeated expression from the graph.
-   * Finally, for each component, it keeps the
-   * first node and removes the rest.
-   */
-  public void removeEquivNodes() {
-    try (Db db = new Db(uri, user, password, database)) {
-      Result res = db.getDistinctPropertyValues("componentId");
-      // Iterate over components
-      while (res.hasNext()) {
-        int component = res.next().get("componentId").asInt();
-        // Remove equivalent nodes from component
-        db.deleteEquivNodes(component);
-        System.out.println("Removed equivalent nodes from component " + component);
-      }
-      // Remove resulting loops
-      db.deleteLoops("Derives");
-      // Delete componentId property
-      db.deleteProperty("componentId");
-    }
-  }
-
-  /**
-   * Creates edges between nodes with the same property. Then, runs the
-   * Weakly Connected Components algorithm to find the connected components
-   * of the graph. Each node gets a componentId property with the id of the
-   * component it belongs to.
-   */
-  public void aggregateEquivNodes(String property) {
-    String projectionName = "equalGraph";
-    String relName = "EQUAL";
-    try (Db db = new Db(uri, user, password, database)) {
-      // Create edges between nodes with the same property
-      db.runQuery("""
-              MATCH (n:Submission)
-              MATCH (s:Submission)
-              WHERE n._id <> s._id AND n.%s = s.%s
-              MERGE (n)-[:%s]-(s)""".formatted(property, property, relName));
-      // Check if graph already exists
-      if (!db.hasProjection(projectionName)) {
-        // Create graph projection
-        db.addProjection(projectionName, "Submission", relName);
-      }
-      // Run Weakly Connected Components algorithm
-      db.runConnectedComponents(projectionName, "componentId");
-      // Drop graph projection
-      db.deleteProjection(projectionName);
-      // Drop edges
-      db.deleteEdges(relName);
-    }
-  }
-
   /**
    * Creates a new database and sets up the graph.
    */
