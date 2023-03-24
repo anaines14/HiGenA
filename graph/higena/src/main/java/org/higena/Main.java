92790ebfd1331228d72f4757353e55b2465624f6
Ana InAas Oliveira de Barros <up201806593@fe.up.pt>
2023-03-24 00:03:09 +0000

    ✨ (Add): interactive menu

--- graph/higena/src/main/java/org/higena/Main.java
+++ graph/higena/src/main/java/org/higena/Main.java
@@ -3,13 +3,41 @@ package org.higena;
 import org.higena.graph.Graph;
 import org.higena.graph.Hint;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    Graph graph = new Graph("9jPK8KBWzjFmBx4Hb", "prop1");
-    //graph.setup();
+    Scanner scanner = new Scanner(System.in);
+    boolean running = true;
+    System.out.println("Welcome to Higena!\n");
 
-    Hint hint = graph.getTEDHint("always (no File and no Protected)");
-    System.out.println("Hint:\n" + hint.toHintMsg());
+    while (running) {
+      showMenu();
+      switch (scanner.nextInt()) {
+        case 1 -> {
+          //execSetup();
+          System.out.println("Setup database");
+        }
+        case 2 -> {
+          //execHint();
+          System.out.println("Get hint");
+        }
+        case 3 -> {
+          running = false;
+          System.out.println("Bye!");
+        }
+        default -> {
+          System.out.println("Invalid option. Try again.");
+        }
+      }
+    }
+  }
 
+
+  private static void showMenu() {
+    System.out.println("Select an option:");
+    System.out.println("1. Setup database");
+    System.out.println("2. Get hint");
+    System.out.println("3. Exit");
   }
 }
