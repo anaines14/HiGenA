4b2c8a92476e0d5c01c74b28d1eb5b5b59762788
Ana Barros <up201806593@fe.up.pt>
2023-03-28 16:45:01 +0100

    🦺 (Update): AST parser according to teacher request: separate declarations

--- data_preparation/parser/src/org/higena/A4FParser.java
+++ data_preparation/parser/src/org/higena/A4FParser.java
@@ -1,214 +1,222 @@
 package org.higena;
 
 import edu.mit.csail.sdg.alloy4.A4Reporter;
-import edu.mit.csail.sdg.alloy4.ConstList;
 import edu.mit.csail.sdg.alloy4.SafeList;
 import edu.mit.csail.sdg.ast.*;
 import edu.mit.csail.sdg.parser.CompModule;
 import edu.mit.csail.sdg.parser.CompUtil;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class A4FParser {
-    public static HashMap<String, String> variables = new HashMap<>();
+  public static HashMap<String, String> variables = new HashMap<>();
 
-    public static A4FNode parse(String exprStr, CompModule module) {
-        variables.clear();
-        Expr expr = CompUtil.parseOneExpression_fromString(module, exprStr);
-        A4FNode tree = parse(expr);
-        return tree == null ? null : Canonicalizer.canonicalize(tree);
-    }
+  public static A4FNode parse(String exprStr, CompModule module) {
+    variables.clear();
+    Expr expr = CompUtil.parseOneExpression_fromString(module, exprStr);
+    A4FNode tree = parse(expr);
+    return tree == null ? null : Canonicalizer.canonicalize(tree);
+  }
 
-    public static A4FNode parse(String code, String func) {
-        variables.clear();
-        // Parse the full module
-        CompModule module = CompUtil.parseEverything_fromString(new A4Reporter(), code);
+  public static A4FNode parse(String code, String func) {
+    variables.clear();
+    // Parse the full module
+    CompModule module = CompUtil.parseEverything_fromString(new A4Reporter(), code);
 
-        // Parse each function
-        SafeList<Func> functions = module.getAllFunc();
-        String funcName;
-        for (Func function : functions) {
-            if (!function.label.contains("Default")) { // Skip default function
-                funcName = function.label.replace("this/", ""); // remove prefix
-                if (funcName.equals(func)) {
-                    A4FNode tree = parse(function.getBody());
-                    return tree == null ? null : Canonicalizer.canonicalize(tree);
-                }
-            }
+    // Parse each function
+    SafeList<Func> functions = module.getAllFunc();
+    String funcName;
+    for (Func function : functions) {
+      if (!function.label.contains("Default")) { // Skip default function
+        funcName = function.label.replace("this/", ""); // remove prefix
+        if (funcName.equals(func)) {
+          A4FNode tree = parse(function.getBody());
+          return tree == null ? null : Canonicalizer.canonicalize(tree);
         }
-        return null;
+      }
+    }
+    return null;
+  }
+
+  private static A4FNode parse(Expr expr) {
+
+    // Parse the expression based on its type
+    switch (expr.getClass().getSimpleName()) {
+      case "ExprConstant":
+        return parse((ExprConstant) expr);
+      case "ExprUnary":
+        return parse((ExprUnary) expr);
+      case "ExprQt":
+        return parse((ExprQt) expr);
+      case "ExprVar":
+        return parse((ExprVar) expr);
+      case "ExprBinary":
+        return parse((ExprBinary) expr);
+      case "PrimSig":
+        return parse((Sig.PrimSig) expr);
+      case "SubsetSig":
+        return parse((Sig.SubsetSig) expr);
+      case "ExprList":
+        return parse((ExprList) expr);
+      case "ExprITE":
+        return parse((ExprITE) expr);
+      case "ExprCall":
+        return parse((ExprCall) expr);
+      case "Field":
+        return parse((Sig.Field) expr);
+      case "ExprLet":
+        return parse((ExprLet) expr);
     }
 
-    private static A4FNode parse(Expr expr) {
+    return null;
+  }
 
-        // Parse the expression based on its type
-        switch (expr.getClass().getSimpleName()) {
-            case "ExprConstant":
-                return parse((ExprConstant) expr);
-            case "ExprUnary":
-                return parse((ExprUnary) expr);
-            case "ExprQt":
-                return parse((ExprQt) expr);
-            case "ExprVar":
-                return parse((ExprVar) expr);
-            case "ExprBinary":
-                return parse((ExprBinary) expr);
-            case "PrimSig":
-                return parse((Sig.PrimSig) expr);
-            case "SubsetSig":
-                return parse((Sig.SubsetSig) expr);
-            case "ExprList":
-                return parse((ExprList) expr);
-            case "ExprITE":
-                return parse((ExprITE) expr);
-            case "ExprCall":
-                return parse((ExprCall) expr);
-            case "Field":
-                return parse((Sig.Field) expr);
-            case "ExprLet":
-                return parse((ExprLet) expr);
-        }
+  private static A4FNode parse(ExprLet expr) {
+    String name = "let";
+    List<A4FNode> children = new ArrayList<>();
 
-        return null;
+    children.add(parse(expr.var));
+    children.add(parse(expr.sub));
+
+    return new A4FNode(name, children);
+  }
+
+  private static A4FNode parse(Sig.Field expr) {
+    String name = "field/" + expr.label;
+    List<A4FNode> children = new ArrayList<>();
+
+    children.add(parse(expr.decl().expr));
+
+    return new A4FNode(name, children);
+  }
+
+  private static A4FNode parse(ExprCall expr) {
+    String name = "field";
+    List<A4FNode> children = new ArrayList<>();
+
+    // Parse the function
+    children.add(parse(expr.fun.getBody()));
+    // Parse the arguments
+    expr.args.forEach(child -> children.add(parse(child)));
+
+    return new A4FNode(name, children);
+  }
+
+  private static A4FNode parse(ExprITE expr) {
+    String name = "ite";
+    List<A4FNode> children = new ArrayList<>();
+
+    children.add(parse(expr.right));
+    children.add(parse(expr.left));
+
+    return new A4FNode(name, children);
+  }
+
+  private static A4FNode parse(ExprList expr) {
+    String name = expr.op.toString();
+    List<A4FNode> children = new ArrayList<>();
+
+    expr.args.forEach(child -> children.add(parse(child)));
+
+    return new A4FNode(name, children);
+  }
+
+  private static A4FNode parse(Sig.SubsetSig expr) {
+    return new A4FNode(expr.toString());
+  }
+
+  private static A4FNode parse(Sig.PrimSig expr) {
+    return new A4FNode(expr.toString());
+  }
+
+  private static A4FNode parse(ExprBinary expr) {
+    String name = expr.op.toString();
+
+    // Parse left and right children
+    List<A4FNode> children = new ArrayList<>();
+    children.add(parse(expr.left));
+    children.add(parse(expr.right));
+
+    return new A4FNode(name, children);
+  }
+
+  private static A4FNode parse(ExprVar expr) {
+    String type = expr.type().toString();
+    String name = variables.get(expr.label) + '/' + type.substring(1,
+            type.length() - 1);
+
+    return new A4FNode(name);
+  }
+
+  private static A4FNode parse(ExprConstant expr) {
+    return new A4FNode(expr.toString());
+  }
+
+  private static A4FNode parse(ExprUnary expr) {
+    return parse(expr, null);
+  }
+
+  private static A4FNode parse(ExprUnary expr, String var) {
+    List<A4FNode> children = new ArrayList<>();
+
+    if (expr.op == ExprUnary.Op.NOOP) {
+      // Skip to children
+      A4FNode child = parse(expr.sub);
+      return new A4FNode(child.getName(), child.getChildren());
     }
 
-    private static A4FNode parse(ExprLet expr) {
-        String name = "let";
-        List<A4FNode> children = new ArrayList<>();
+    // Parse the children
+    String name = expr.op.toString();
+    if (var != null) {
+      addVariable(var);
+      children.add(new A4FNode(variables.get(var)));
+    }
+    children.add(parse(expr.sub));
+    return new A4FNode(name, children);
+  }
 
-        children.add(parse(expr.var));
+
+  private static A4FNode parse(ExprQt expr) {
+    String name = expr.op.toString();
+    List<A4FNode> children = new ArrayList<>();
+
+    if (!expr.decls.isEmpty()) {
+      Decl decl = expr.decls.get(0);
+      if (decl.expr instanceof ExprUnary && !decl.names.isEmpty()) {
+        children.add(parse((ExprUnary) decl.expr, decl.names.get(0).label));
+      }
+
+      if (decl.names.size() > 1) {
+        children.add(parse(expr, 1));
+      } else {
         children.add(parse(expr.sub));
-
-        return new A4FNode(name, children);
+      }
+      return new A4FNode(name, children);
     }
+    // Parse the body
+    children.add(parse(expr.sub));
+    return new A4FNode(name, children);
+  }
 
-    private static A4FNode parse(Sig.Field expr) {
-        String name = "field/" + expr.label;
-        List<A4FNode> children = new ArrayList<>();
+  private static A4FNode parse(ExprQt expr, int varIndex) {
+    String name = expr.op.toString();
+    List<A4FNode> children = new ArrayList<>();
 
-        children.add(parse(expr.decl().expr));
+    Decl decl = expr.decls.get(0);
+    children.add(parse((ExprUnary) decl.expr, decl.names.get(varIndex).label));
 
-        return new A4FNode(name, children);
+    if (decl.names.size() > varIndex + 1) {
+      children.add(parse(expr, varIndex + 1));
+    } else {
+      children.add(parse(expr.sub));
     }
+    return new A4FNode(name, children);
+  }
 
-    private static A4FNode parse(ExprCall expr) {
-        String name = "field";
-        List<A4FNode> children = new ArrayList<>();
-
-        // Parse the function
-        children.add(parse(expr.fun.getBody()));
-        // Parse the arguments
-        expr.args.forEach(child -> children.add(parse(child)));
-
-        return new A4FNode(name, children);
-    }
-
-    private static A4FNode parse(ExprITE expr) {
-        String name = "ite";
-        List<A4FNode> children = new ArrayList<>();
-
-        children.add(parse(expr.right));
-        children.add(parse(expr.left));
-
-        return new A4FNode(name, children);
-    }
-
-    private static A4FNode parse(ExprList expr) {
-        String name = expr.op.toString();
-        List<A4FNode> children = new ArrayList<>();
-
-        expr.args.forEach(child -> children.add(parse(child)));
-
-        return new A4FNode(name, children);
-    }
-
-    private static A4FNode parse(Sig.SubsetSig expr) {
-        return new A4FNode(expr.toString());
-    }
-
-    private static A4FNode parse(Sig.PrimSig expr) {
-        return new A4FNode(expr.toString());
-    }
-
-    private static A4FNode parse(ExprBinary expr) {
-        String name = expr.op.toString();
-
-        // Parse left and right children
-        List<A4FNode> children = new ArrayList<>();
-        children.add(parse(expr.left));
-        children.add(parse(expr.right));
-
-        return new A4FNode(name, children);
-    }
-
-    private static A4FNode parse(ExprVar expr) {
-        String type = expr.type().toString();
-        String name = variables.get(expr.label) +'/' + type.substring(1,
-                type.length() - 1);
-
-        return new A4FNode(name);
-    }
-
-    private static A4FNode parse(ExprConstant expr) {
-        return new A4FNode(expr.toString());
-    }
-
-    private static A4FNode parse(ExprUnary expr) {
-        return parse(expr, null);
-    }
-
-    private static A4FNode parse(ExprUnary expr, A4FNode vars) {
-        List<A4FNode> children = new ArrayList<>();
-
-        if (expr.op == ExprUnary.Op.NOOP) {
-            // Skip to children
-            A4FNode child = parse(expr.sub);
-            return new A4FNode(child.getName(), child.getChildren());
-        }
-
-        // Parse the children
-        String name = expr.op.toString();
-        if (vars != null)
-            children.add(vars);
-        children.add(parse(expr.sub));
-        return new A4FNode(name, children);
-    }
-
-    private static A4FNode parse(ConstList<? extends ExprHasName> names) {
-        if (names.isEmpty())
-            return null;
-
-        List<A4FNode> children = new ArrayList<>();
-
-        for (ExprHasName name : names) {
-            addVariable(name.label);
-            children.add(new A4FNode(variables.get(name.label)));
-        }
-
-        return new A4FNode("vars", children);
-    }
-
-    private static A4FNode parse(ExprQt expr) {
-        String name = expr.op.toString();
-        List<A4FNode> children = new ArrayList<>();
-
-        // Parse the declarations
-        for (Decl decl : expr.decls) {
-            if (decl.expr instanceof ExprUnary) {
-               children.add(parse((ExprUnary) decl.expr, parse(decl.names)));
-            } else {
-                children.add(parse(decl.expr));
-            }
-        }
-
-        // Parse the body
-        children.add(parse(expr.sub));
-
-        return new A4FNode(name, children);
-    }
-
-    private static void addVariable(String var) {
-        variables.putIfAbsent(var, "var" + variables.size());
-    }
+  private static void addVariable(String var) {
+    variables.putIfAbsent(var, "var" + variables.size());
+  }
 }
 
--- data_preparation/parser/src/tests/A4FParserTest.java
+++ data_preparation/parser/src/tests/A4FParserTest.java
@@ -2,12 +2,13 @@ import org.higena.A4FParser;
 
 public class A4FParserTest {
 
-    public static void main(String[] args) {
-        String code = "var sig File { var link : lone File } var sig Trash in" +
-                " File {} var sig Protected in File {} pred prop1 {all x:File" +
-                " | some y:File | x in y.link } pred prop2 { all b:File | " +
-                "some n:File | n in b.link } pred prop3 { some File.link}";
-        System.out.println(A4FParser.parse(code, "prop1"));
-        System.out.println(A4FParser.parse(code, "prop2"));
-    }
+  public static void main(String[] args) {
+    String code = "var sig File { var link : lone File } var sig Trash in" + " File {} var sig Protected in File {} pred prop1 {all x:File" + " | some y:File | x in y.link } pred prop2 { all b:File | " + "some n:File | n in b.link } pred prop3 { some File.link}" + " pred prop4 { all x,y:File | x in y.link } pred prop5 {no Trash or all f : File | f in Trash}";
+
+    System.out.println(A4FParser.parse(code, "prop1"));
+    System.out.println(A4FParser.parse(code, "prop2"));
+    System.out.println(A4FParser.parse(code, "prop3"));
+    System.out.println(A4FParser.parse(code, "prop4"));
+    System.out.println(A4FParser.parse(code, "prop5"));
+  }
 }
