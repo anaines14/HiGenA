1c5050ca323f7ae83a416ebabb9a57d43b020bc5
Ana Barros <up201806593@fe.up.pt>
2023-05-02 18:15:59 +0100

    ♻️ (Refactor): action tests

--- graph/higena/src/test/java/ActionsTest.java
+++ graph/higena/src/test/java/ActionsTest.java
@@ -10,28 +10,40 @@ import java.util.stream.Stream;
 
 public class ActionsTest {
 
-  @ValueSource(strings = {"{type='Delete', node='}"})
+  @ValueSource(strings = {
+          "{type='Delete', node='}",
+          "{type='TreeDelete', tree='{AND{no{Protected}}{no{Trash}}}'",
+          "{type='TreeInsert', tree='{AND{no{Protected}}{no{Trash}}}', " +
+                  "parent='root', position=0}",
+
+  })
   @ParameterizedTest
   public void parseAction(String actionStr) {
     System.out.println(EditAction.fromString(actionStr));
-
   }
 
   @MethodSource("astInputsProvider")
   @ParameterizedTest
   public void printTreeDiffs(String ast1, String ast2) {
     TED ted = new TED();
-    TreeDiff diff = ted.computeAptedTreeDiff(ast1, ast2);
+    TreeDiff diff = ted.computeGumTreeTreeDiff(ast1, ast2);
 
     for (EditAction action : diff.getActions()) {
       System.out.println("Action: " + action);
-      System.out.println("Hint: " + EditAction.fromString(action.toString()));
     }
   }
 
   private static Stream<Arguments> astInputsProvider() {
     return Stream.of(
+            // TreeInsert
+            Arguments.of("", "{AND{no{Protected}}}"),
+            // Move
+            Arguments.of("{<{Protected}{no}}", "{<{no{Protected}}}"),
+            // Update
             Arguments.of("{AND{no{Protected}}{no{Trash}}}", "{AND{no{File" +
-                    "}}{no{Trash}}}"));
+                    "}}{no{Trash}}}"),
+            // TreeDelete
+            Arguments.of("{AND{no{Protected}}}", "{AND}"));
+
   }
 }
