d9debd34bbb830c7d570d8a4a9d4bde5c50692f5
Ana Barros <up201806593@fe.up.pt>
2023-05-02 16:18:25 +0100

    ♻️ (Refactor ): hint gen tests into separate tests

--- graph/higena/src/test/java/HintTest.java
+++ graph/higena/src/test/java/HintTest.java
@@ -2,10 +2,8 @@ import org.higena.graph.Graph;
 import org.higena.graph.hint.HintGenType;
 import org.higena.graph.hint.HintGenerator;
 import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,16 +13,74 @@ import java.util.*;
 import java.util.stream.Stream;
 
 public class HintTest {
-  private static File file = null;
-  private static final boolean statistics = true;
+  private static final Stream<Arguments> test_data = testDataProvider();
 
-  @BeforeAll
-  public static void setup() {
-    if (statistics)
-      createFile("hints_statistics", "src/test/outputs/");
+  // Tests
+
+  @Test
+  public void testNoNewPathsHintGen() {
+    // Create log file
+    File logFile = createLogFile("no_new_paths_hint_stats");
+
+    // Set hint generation settings
+    HintGenerator.cantCreatePath = true;
+
+    test_data.forEach(args -> {
+      String challenge = (String) args.get()[0], predicate = (String) args.get()[1];
+      File test_data = (File) args.get()[2];
+      genHints(challenge, predicate, test_data, logFile);
+    });
   }
 
-  // Test method
+  @Test
+  public void testAptedHintGen() {
+    // Create log file
+    File logFile = createLogFile("apted_hint_stats");
+
+    // Set hint generation settings
+    // TODO
+
+    test_data.forEach(args -> {
+      String challenge = (String) args.get()[0], predicate = (String) args.get()[1];
+      File test_data = (File) args.get()[2];
+      genHints(challenge, predicate, test_data, logFile);
+
+    });
+  }
+
+  @Test
+  public void testGumTreeHintGen() {
+    // Create log file
+    File logFile = createLogFile("gumtree_hint_stats");
+
+    // Set hint generation settings
+    // TODO
+
+    test_data.forEach(args -> {
+      String challenge = (String) args.get()[0], predicate = (String) args.get()[1];
+      File test_data = (File) args.get()[2];
+      genHints(challenge, predicate, test_data, logFile);
+    });
+  }
+
+  private void genHints(String challenge, String predicate, File test_data,
+                       File logFile) {
+    // Load train data
+    Graph g = new Graph(challenge, predicate);
+    // Load test data
+    List<Map.Entry<String, String>> expressions = getTestSubmissions(test_data);
+    // Generate hints
+    for (Map.Entry<String, String> entry : expressions) {
+      g.setup(); // Reset graph for each submission
+      String expr = entry.getKey(), code = entry.getValue();
+      // Generate hint
+      HintGenerator hintGen = g.generateHint(expr, code, HintGenType.TED);
+      // Log hint
+      writeStatistics(logFile, hintGen, challenge, predicate);
+    }
+  }
+
+  // data providers
 
   private static Stream<Arguments> testDataProvider() {
     String test_dir = "../data/test";
@@ -39,61 +95,13 @@ public class HintTest {
         String predicate = predicate_file.getName().replace(".json", "");
         // Append arguments to stream (challenge, predicate, predicate file
         // with test data)
-        stream = Stream.concat(stream, Stream.of(Arguments.of(challenge,
-                predicate, predicate_file)));
+        stream = Stream.concat(stream, Stream.of(Arguments.of(challenge, predicate, predicate_file)));
       }
     }
     return stream;
   }
 
-  public static void createFile(String name, String path) {
-    file = new File(path + name + ".json");
-    file.delete();
-  }
-
-  // Auxiliary methods
-
-  public static void writeLineToFile(String line) {
-    try {
-      FileWriter writer = new FileWriter(file, true);
-      writer.write(line + '\n');
-      writer.close();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  // Logging methods
-
-  public static void writeStatistics(HintGenerator hintGen, String challenge,
-                                     String predicate) {
-    JSONObject obj = hintGen.getJSON();
-    obj.put("challenge", challenge);
-    obj.put("predicate", predicate);
-    String row = obj.toString();
-    writeLineToFile(row);
-  }
-
-  @MethodSource("testDataProvider")
-  @ParameterizedTest
-  public void testHintGen(String challenge, String predicate, File test_data) {
-    // Load train data
-    Graph g = new Graph(challenge, predicate);
-    // Load test data
-    List<Map.Entry<String, String>> expressions = getTestData(test_data);
-    // Generate hints
-    for (Map.Entry<String, String> entry : expressions) {
-      g.setup(); // Reset graph
-      String expr = entry.getKey(), code = entry.getValue();
-      // Generate hint
-      HintGenerator hintGen = g.generateHint(expr, code, HintGenType.TED);
-      // Log hint if analytics enabled
-      if (statistics)
-        writeStatistics(hintGen, challenge, predicate);
-    }
-  }
-
-  private List<Map.Entry<String, String>> getTestData(File data_file) {
+  private List<Map.Entry<String, String>> getTestSubmissions(File data_file) {
     List<Map.Entry<String, String>> submissions = new ArrayList<>();
 
     // Parse JSON file
@@ -115,4 +123,33 @@ public class HintTest {
 
     return submissions;
   }
+
+  // Logging methods
+
+  public static void writeLineToFile(File file, String line) {
+    try {
+      FileWriter writer = new FileWriter(file, true);
+      writer.write(line + '\n');
+      writer.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void writeStatistics(File logFile, HintGenerator hintGen,
+                                     String challenge, String predicate) {
+    JSONObject obj = hintGen.getJSON();
+    obj.put("challenge", challenge);
+    obj.put("predicate", predicate);
+    String row = obj.toString();
+    writeLineToFile(logFile, row);
+  }
+
+  public File createLogFile(String name) {
+    String PATH = "src/test/outputs/";
+    File file = new File(PATH + name + ".json");
+    file.delete(); // Delete file if it already exists
+    return file;
+  }
+
 }
