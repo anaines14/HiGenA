0c61a7f4fab2aeeb4f9b288180fa2558c54f23e6
Ana Barros <up201806593@fe.up.pt>
2023-05-17 15:21:49 +0100

    ♻️ (Refactor): Pass expr parser to higena

---
+++ higena/src/main/java/org/higena/parsers/ExprExtractor.java
@@ -0,0 +1,63 @@
+package org.higena.parsers;
+
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Extracts the text between curly braces of an Alloy predicate.
+ */
+public class ExprExtractor {
+
+  private final String code;
+  private final Stack<Character> stack;
+
+  public ExprExtractor(String code) {
+    this.code = code;
+    stack = new Stack<>();
+  }
+
+  public String parse(String predicateName) {
+    StringBuilder expr = new StringBuilder();
+    boolean inExpr = false;
+    // Find the predicate
+    int exprIndex =
+            findWordIndex(predicateName, code) + predicateName.length();
+
+    if (exprIndex == -1) {
+      System.err.println("ERROR: Predicate not found.");
+      return null;
+    }
+
+    // Parse the predicate
+    for (int i = exprIndex; i < code.length(); i++) {
+      char c = code.charAt(i);
+      if (c == '}') {
+        stack.pop();
+        if (stack.isEmpty()) {
+          return expr.toString();
+        }
+      }
+      if (inExpr) {
+        expr.append(c);
+      }
+      if (c == '{') {
+        inExpr = true;
+        stack.push(c);
+      }
+    }
+    return expr.toString();
+  }
+
+  private int findWordIndex(String word, String line) {
+    String regex = word + "\\s*\\{";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(line);
+
+    if (matcher.find()) {
+      return matcher.start();
+    }
+
+    return -1;
+  }
+}
---
+++ higena/src/test/java/ExprExtractorTest.java
@@ -0,0 +1,16 @@
+import org.higena.parsers.ExprExtractor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExprExtractorTest {
+
+  @Test
+  public void testExtraction() {
+    String code = "abstract sig Source {} sig User extends Source { profile : set Work, visible : set Work } sig Institution extends Source {} sig Id {} sig Work { ids : some Id, source : one Source } pred Inv1 { } pred Inv1o { all u : User | u.visible in u.profile } check Inv1OK { (Inv2o and Inv3o and Inv4o and (some ShouldBeRejected iff (Inv1 and not Inv1o))) implies (Inv1 iff Inv1o) } pred Inv2 { } pred Inv2o { all u : User | u.profile.source in Institution+u } check Inv2OK { (Inv1o and Inv3o and Inv4o and (some ShouldBeRejected iff (Inv2 and not Inv2o))) implies (Inv2 iff Inv2o) } pred Inv3 { } pred Inv3o { all u : User, disj x,y : u.profile | x.source = y.source implies no (x.ids & y.ids) } check Inv3OK { (Inv1o and Inv2o and Inv4o and (some ShouldBeRejected iff (Inv3 and not Inv3o))) implies (Inv3 iff Inv3o) } pred Inv4 { } pred Inv4o { all u : User, disj x,y : u.visible | x not in y.^(ids.~ids) } check Inv4OK { (Inv1o and Inv2o and Inv3o and (some ShouldBeRejected iff (Inv4 and not Inv4o))) implies (Inv4 iff Inv4o) } abstract one sig RejectedBy {} sig ShouldBeRejected extends RejectedBy {} sig ShouldBeAccepted extends RejectedBy {}";
+    ExprExtractor parser = new ExprExtractor(code);
+    String expr = parser.parse("Inv2");
+
+    assertTrue(expr.trim().isEmpty());
+  }
+}
